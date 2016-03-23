7cae81e0c6acfe8f254d45dbc151cdc5cbe19d49
zf2timo <timo.tewes@gmail.com>
2016-03-23 20:46:11 +0100

    Added HydratorManagerFactory

---
+++ src/Bridge/Factory/HydratorManagerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace TTe\Bridge\Factory;
+
+
+use Zend\Hydrator\HydratorPluginManager;
+
+class HydratorManagerFactory
+{
+    const PLUGIN_MANAGER_CLASS = HydratorPluginManager::class;
+}
